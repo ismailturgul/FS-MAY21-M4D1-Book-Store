e6898abe897fc397b58f4aed2d51aff79f89a1fe
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-07-06 16:29:55 +0200

    Book-store
    Added Background-picture

--- book-store/src/Jumbotron.jsx
+++ book-store/src/Jumbotron.jsx
@@ -1,8 +1,9 @@
 import {Jumbotron} from "react-bootstrap"
 
 const MyJumbotron = () => (
-<Jumbotron classname="img">
+<Jumbotron>
   <h1>Hello Strive-School!</h1>
+  <img src="https://media.istockphoto.com/photos/learn-spanish-concept-spanish-dictionary-book-or-textbok-with-flag-of-picture-id1005291410" width={500} alt="" />
   <p>
     Welcome to the Isi-est Book Store ever.
   </p>
